b1141986654f4601ec610caae9be03a7b2efbc8c
lemonliu1993 <2305247091@qq.com>
2024-06-22 08:59:54 +0800

    创建es客户端

---
+++ src/main/java/com/lemon/es/test/ESTest_Client.java
@@ -0,0 +1,24 @@
+package com.lemon.es.test;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.transport.TransportClient;
+
+import java.io.IOException;
+
+/**
+ * Created by lemoon on 2024/6/22 08:56
+ */
+public class ESTest_Client {
+    public static void main(String[] args) throws IOException {
+
+        //创建ES客户端
+        RestHighLevelClient esClient = new RestHighLevelClient(
+                RestClient.builder(new HttpHost("localhost", 9200, "http"))
+        );
+
+        //关闭ES客户端
+        esClient.close();
+    }
+}
